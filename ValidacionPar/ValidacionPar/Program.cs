92b7cfbf001532ae94a4ea935860497993080559
ricardo taborda palacio <richtp@hotmail.com>
2023-04-06 11:21:29 -0500

    punto 2 del taller

---
+++ ValidacionPar/ValidacionPar/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ValidacionPar
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n, swth = 1;
+            string res;
+            while (swth == 1)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("****** Validacion SI el Numero es Par o NO  *****   \n");
+                Console.WriteLine("     Ingresa El Numero Por Favor ");
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n % 2 == 0)
+                {
+                    res = "Par";
+                    Console.WriteLine(" El Numero es " + res);
+                }
+                else
+                {
+                    res = "Impar";
+                    Console.WriteLine(" El Numero es " + res);
+                }
+                Console.WriteLine("\n para ingresar otro numero marque 1 o cualquiera para finalizar");
+                swth = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+            }
+        }
+    }
+}
