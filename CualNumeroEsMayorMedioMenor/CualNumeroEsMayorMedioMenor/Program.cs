143f1b5b4e2afd98baf21f6ee97b16fbdd7bbd20
ricardo taborda palacio <richtp@hotmail.com>
2023-04-06 14:22:35 -0500

    nuevo int

---
+++ CualNumeroEsMayorMedioMenor/CualNumeroEsMayorMedioMenor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CualNumeroEsMayorMedioMenor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, num3, swth = 1;
+
+            Console.WriteLine("\n ****** Cual Numero Es Mayor, Medio y El Menor de los Ingresados  ******\n");
+            Console.WriteLine("                Ingresa Por Favor 3 Numeros\n");
+
+        }
+    }
+}
