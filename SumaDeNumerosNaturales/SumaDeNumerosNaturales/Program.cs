cd652f4a4c82f0e67a12c935e65c12f5f89adfbe
ricardo taborda palacio <richtp@hotmail.com>
2023-04-09 17:13:09 -0500

    Nuevo Punto Listo

---
+++ SumaDeNumerosNaturales/SumaDeNumerosNaturales/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SumaDeNumerosNaturales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i,swth=1;
+            while (swth == 1)
+            {
+                int suma = 0;
+                Console.WriteLine("\n ****** Suma de Numeros Naturales hasta el Numero Ingresado *****   \n");
+                Console.WriteLine("              Ingresa Por favor Un Numero Natural");
+                n = Convert.ToInt32(Console.ReadLine());
+                for (i = 1; i <= n; i++)
+                {
+                    suma = suma + i;
+                }
+
+                Console.WriteLine(string.Format("\nLa suma es: {0} ", suma));
+
+                Console.WriteLine("Si Desea hacer Otra Suma Marque 1 o Cualquiera Para Finalizar");
+                swth = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+            }
+        }
+    }
+}
