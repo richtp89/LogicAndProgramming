14e61c795914a1e6feeec50ad9c27c821fa2e4ad
ricardo taborda palacio <richtp@hotmail.com>
2023-04-24 21:46:51 -0500

    ahora si

--- LosPrimos/LosPrimos/Program.cs
+++ LosPrimos/LosPrimos/Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
 
-namespace LosPrimos
+namespace NumerosPrimosHasta
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n, swth = 1, divs = 0;
+
+            while (swth == 1)
+            {
+                int j = 2;
+                Console.WriteLine("\n  ****  Programa Para Mostrar Los Numeros Primos Hasta el Ingresado  **** \n");
+                Console.WriteLine("                    Ingresa Por Favor Hasta que Numero");
+                n = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(" ");
+                if (n > 2)
+                {
+                    while (j <= n)
+                    {
+                        for (int i = 1; i <= j; i++)
+                        {
+                            if (j % i == 0) divs++;
+                            if (divs > 2) break;
+                        }
+
+                        if (divs == 2) Console.WriteLine(string.Format("     {0}", j));
+                        divs = 0;
+                        j++;
+                    }
+
+                    Console.WriteLine("\n Para Ingresar Un Nuevo Numero Marca 1 o Cualquiera Para Finalizar ");
+                    swth = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.WriteLine(" Por Favor Ingresar un Numero Mayor que 2 ");
+                    Console.WriteLine("\n Para Ingresar Un Nuevo Numero Marca 1 o Cualquiera Para Finalizar ");
+                    swth = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                }
+            }
         }
     }
 }
